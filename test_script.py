2639c8a9a08aab90ac6421114ddac30a5d2f40a0
Jonathan Daniel <jmdanie@login001.palmetto.clemson.edu>
2020-09-12 17:57:25 -0400

    Added Python Scripts from Palmetto

---
+++ test_script.py
@@ -0,0 +1,114 @@
+import tensorflow as tf
+import ast
+
+import numpy as np
+import pandas as pd
+
+import os
+import time
+
+df = pd.read_csv('data/RAW_recipes.csv')
+recipe_df = df[["steps"]]
+
+recipe_df["processed"] = [". ".join(ast.literal_eval(step_array)) for step_array in recipe_df.steps]
+text = ""
+
+for recipe in recipe_df["processed"]:
+    text += recipe
+
+# The unique characters in the file
+vocab = sorted(set(text))
+print ('{} unique characters'.format(len(vocab)))
+
+# Creating a mapping from unique characters to indices
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+text_as_int = np.array([char2idx[c] for c in text])
+
+print('{')
+for char,_ in zip(char2idx, range(20)):
+    print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
+print('  ...\n}')
+
+# Show how the first 13 characters from the text are mapped to integers
+print ('{} ---- characters mapped to int ---- > {}'.format(repr(text[:13]), text_as_int[:13]))
+
+# The maximum length sentence we want for a single input in characters
+seq_length = 100
+examples_per_epoch = len(text)//(seq_length+1)
+
+# Create training examples / targets
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+for i in char_dataset.take(5):
+  print(idx2char[i.numpy()])
+
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+
+for item in sequences.take(5):
+  print(repr(''.join(idx2char[item.numpy()])))
+
+checkpoint_dir = './training_checkpoints'
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                              batch_input_shape=[batch_size, None]),
+    tf.keras.layers.LSTM(rnn_units,
+                        return_sequences=True,
+                        stateful=True,
+                        recurrent_initializer='glorot_uniform'),
+    tf.keras.layers.Dense(vocab_size)
+  ])
+  return model
+
+
+tf.train.latest_checkpoint(checkpoint_dir)
+
+model = build_model(67, 256, 1024, batch_size=1)
+
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+
+model.build(tf.TensorShape([1, None]))
+
+model.summary()
+
+def generate_text(model, start_string):
+  # Evaluation step (generating text using the learned model)
+
+  # Number of characters to generate
+  num_generate = 1000
+
+  # Converting our start string to numbers (vectorizing)
+  input_eval = [char2idx[s] for s in start_string]
+  input_eval = tf.expand_dims(input_eval, 0)
+
+  # Empty string to store our results
+  text_generated = []
+
+  # Low temperatures results in more predictable text.
+  # Higher temperatures results in more surprising text.
+  # Experiment to find the best setting.
+  temperature = 1.0
+
+  # Here batch size == 1
+  model.reset_states()
+  for i in range(num_generate):
+    predictions = model(input_eval)
+    # remove the batch dimension
+    predictions = tf.squeeze(predictions, 0)
+
+    # using a categorical distribution to predict the character returned by the model
+    predictions = predictions / temperature
+    predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+
+    # We pass the predicted character as the next input to the model
+    # along with the previous hidden state
+    input_eval = tf.expand_dims([predicted_id], 0)
+
+    text_generated.append(idx2char[predicted_id])
+
+  return (start_string + ''.join(text_generated))
+
+print(generate_text(model, start_string=u"add"))
